x2nie/webamp-core

commit 9fa7a7893f0c3d3d2613c5b36faf418c362dd82b

Don't error when trying to play an empty track

js/mediaMiddleware.js
@@ -93,17 +93,13 @@ export default media => store => {
         media.seekToPercentComplete(action.percent);
         break;
       case PLAY_TRACK:
-        media.loadFromUrl(
-          store.getState().playlist.tracks[action.id].url,
-          true
-        );
+      case BUFFER_TRACK: {
+        const track = store.getState().playlist.tracks[action.id];
+        if (track != null) {
+          media.loadFromUrl(track.url, true);
+        }
         break;
-      case BUFFER_TRACK:
-        media.loadFromUrl(
-          store.getState().playlist.tracks[action.id].url,
-          false
-        );
-        break;
+      }
       case SET_BAND_VALUE:
         if (action.band === "preamp") {
           media.setPreamp(action.value);

js/actionCreators.js
@@ -82,6 +82,9 @@ const loadQueue = new LoadQueue({ threads: 4 });
 function playRandomTrack() {
   return (dispatch, getState) => {
     const { playlist: { trackOrder, currentTrack } } = getState();
+    if (trackOrder.length === 0) {
+      return;
+    }
     let nextId;
     do {
       nextId = trackOrder[Math.floor(trackOrder.length * Math.random())];

commit a0c67659c44d9812032508cd861f2e757d8a4b46

Don't autoplay on buffer
Fix regression introduced in 9fa7a7893f0c3d3d2613c5b36faf418c362dd82b.

js/mediaMiddleware.js
@@ -93,10 +93,15 @@ export default media => store => {
         media.seekToPercentComplete(action.percent);
         break;
       case PLAY_TRACK:
-      case BUFFER_TRACK: {
         const track = store.getState().playlist.tracks[action.id];
         if (track != null) {
           media.loadFromUrl(track.url, true);
+        }
+        break;
+      case BUFFER_TRACK: {
+        const track = store.getState().playlist.tracks[action.id];
+        if (track != null) {
+          media.loadFromUrl(track.url, false);
         }
         break;
       }