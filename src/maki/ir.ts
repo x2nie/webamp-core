32765f2f98a5247c0e05a22970f1ad3c8e486805
Fathony L <fathony@smart-leaders.net>
2025-02-15 20:13:37 +0700

    preparing to give real IR params

--- src/maki/ir.ts
+++ src/maki/ir.ts
@@ -1,26 +1,28 @@
 import { ASTNode } from "./astTree";
 
 type IRInstruction = string[];
+type Solver = (node:ASTNode) => string;
+const solver = (node:ASTNode) => JSON.stringify(node).replace(/"/g,"'");
 
-export function generateIR(node: ASTNode, ir: IRInstruction = []): IRInstruction {
+export function generateIR(node: ASTNode, solve:Solver=solver, ir: IRInstruction = []): IRInstruction {
     if(!node || !node.type) {
         debugger
     }
 
     if (node.type === "NumberLiteral") {
-        ir.push(`PUSH ${node.value}`);
+        ir.push(`PUSH ${solve(node)}`);
     }
     else if (node.type === "identifier") {
-        ir.push(`PUSH ${node.name}`);
+        ir.push(`PUSH ${solve(node)}`);
     }
     else if (node.type === "UnaryExpr") {
-        generateIR(node.expr, ir);
+        generateIR(node.expr, solve, ir);
         if (node.operator === "-") ir.push("NEG");
         else if (node.operator === "!") ir.push("NOT");
     }
     else if (node.type === "BinaryExpr") {
-        generateIR(node.left, ir);
-        generateIR(node.right, ir);
+        generateIR(node.left, solve, ir);
+        generateIR(node.right, solve, ir);
         const opMap: Record<string, string> = {
             "+": "ADD", "-": "SUB",
             "*": "MUL", "/": "DIV", "%": "MOD",
